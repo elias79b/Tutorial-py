d860d286b371de1939383100b7c7e0f3617ceedc
eliasb79b <eliasbabakhani@gmail.com>
2025-10-21 20:57:55 +0330

    7

--- first.py
+++ first.py
@@ -1,15 +1,22 @@
-print('hello word')
-name = "elyas"
-age = 25
-is_male = True
-print(name,age,is_male)
-print('tarazo\n bojnourd\trrrr ')
+# print('hello word')
+# name = "elyas"
+# age = 25
+# is_male = True
+# print(name,age,is_male)
+# print('tarazo\n bojnourd\trrrr ')
+#
+# print ("esbi \'ssss\'")
+#
+# num1 = 2
+# num3 = 7
+# print (num1+num3)
+# name =  input("hi buddy")
+# print("your name is"+ name)
+a = 90
+b = 30
 
-print ("esbi \'ssss\'")
+if a > b:
+    print ("aaaa")
 
-num1 = 2
-num3 = 7
-print (num1+num3)
-name =  input("hi buddy")
-print("your name is"+ name)
 
+    # /*-+
