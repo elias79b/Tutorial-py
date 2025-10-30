29a82b44dd9c766728957f1c813f1e2f7f7e7dd7
eliasb79b <eliasbabakhani@gmail.com>
2025-10-30 21:01:43 +0330

    10

--- first.py
+++ first.py
@@ -12,16 +12,17 @@
 # print (num1+num3)
 # name =  input("hi buddy")
 # print("your name is"+ name)
-a = 90
-b = 30
+# a = 90
+# b = 30
+#
+# if a > b:
+#     print ("aaaa")
+#
+#
+#     # /*-+
+# list22 = [1,2,3,4,5,5,True,False,'elias']
+# print(list22)
+#
+# lis2 = (1,2,3,4)
+# print(lis2)
 
-if a > b:
-    print ("aaaa")
-
-
-    # /*-+
-list22 = [1,2,3,4,5,5,True,False,'elias']
-print(list22)
-
-lis2 = (1,2,3,4)
-print(lis2)
